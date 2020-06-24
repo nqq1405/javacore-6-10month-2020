8c61c3624f62d4b0b5d38b1de272fdd01dd3dae0
Nguyen Quy Quang <quang20hn@gmail.com>
2020-06-24 18:26:16 +0700

    ac41

--- src/lession4/Activity_41.java
+++ src/lession4/Activity_41.java
@@ -1,4 +1,31 @@
 package lession4;
 
 public class Activity_41 {
+
+    static boolean ktamstrong(int n){
+        int du,tong = 0,demchia = 0;
+        int t = n;
+        while (t!=0){
+            demchia++;
+            t=t/10;
+        }
+        t = n;
+        while (t!=0){
+            du = t%10;
+            tong += (int) Math.pow(du,demchia);
+            t/=10;
+        }
+        if (n == tong)
+            return true;
+        else return false;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 500 ; i++) {
+            if (ktamstrong(i)==true) {
+                System.out.print(i+" ");
+            }
+        }
+//        System.out.println((int) Math.pow(153,3));
+    }
 }
